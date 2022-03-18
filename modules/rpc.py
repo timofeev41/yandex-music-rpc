28822890a2a7b951e5324f898b1d30c2d43cc209
Nikolas Timofeev <timofeevnik41@gmail.com>
2022-03-19 00:55:45 +0300

    :zap: Small fixes + join links

--- modules/rpc.py
+++ modules/rpc.py
@@ -1,26 +1,44 @@
+import typing as tp
+
 from pypresence import Presence  # The simple rich presence client in pypresence
 import time
 
 import modules.yandexmusic as ym
 
-client_id = "954472930530709544"  # Put your Client ID in here
+client_id = "CLIENT ID"  # Put your Client ID in here
 RPC = Presence(client_id)  # Initialize the Presence client
 
 last_track = ym.get_current_playing_track()
 last_track_data = ym.get_track_info(last_track["track_id"])
 
+
 RPC.connect()
 
 
-def _update_status(state: str) -> None:
-    RPC.update(state=state)
+def _update_status(state: str, details: str, song: dict[str, tp.Any]) -> None:
+    RPC.update(
+        state=details,
+        details=state,
+        buttons=[
+            {
+                "label": "Listen on Yandex.Music",
+                "url": f"https://music.yandex.ru/album/{song['album_id']}/track/{song['track_id']}",
+            },
+            {
+                "label": "Album on Yandex.Music",
+                "url": f"https://music.yandex.ru/album/{song['album_id']}",
+            },
+        ],
+    )
 
 
-def poll():
+def poll() -> None:
     global last_track
     global last_track_data
     while True:
-        _update_status(f"Сейчас слушает {last_track_data['title']}-{','.join(last_track_data['artists'])}")
+        _update_status(
+            f"Сейчас слушает:", f"{last_track_data['title']} - {', '.join(last_track_data['artists'])}", last_track
+        )
         time.sleep(15)
         last_track = ym.get_current_playing_track()
         last_track_data = ym.get_track_info(last_track["track_id"])
--- modules/yandexmusic.py
+++ modules/yandexmusic.py
@@ -1,30 +1,29 @@
 import typing as tp
 
-from yandex_music import Client
+from yandex_music import Client, QueueItem, TrackId
 
 
-client = Client(token="YOUR TOKEN").init()
+client = Client(token="TOKEN").init()
 
 
-def get_all_queues() -> dict[str, str]:
+def get_all_queues() -> list[QueueItem]:
     return client.queues_list()
 
 
-def get_latest_queue() -> dict[str, str]:
+def get_latest_queue() -> QueueItem:
     return get_all_queues()[0]
 
 
-def get_current_playing_track() -> dict[str, str]:
+def get_current_playing_track() -> TrackId:
     return get_latest_queue().fetch_queue().get_current_track()
 
 
-def get_track_info(track_id: str) -> dict[str, str]:
+def get_track_info(track_id: str) -> dict[str, tp.Any]:
     track: dict[str, tp.Any] = client.tracks(track_ids=track_id)[0]
     return {
         "title": track["title"],
+        "track_id": track_id,
         "artists": [artist["name"] for artist in track["artists"]],
         "album": track["albums"][-1]["title"],
+        "album_id": track["albums"][-1]["id"],
     }
-
-
-# print(get_track_info(get_current_playing_track()["track_id"]))
